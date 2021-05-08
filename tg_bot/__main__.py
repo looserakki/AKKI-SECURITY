65feb1af2c8fd3234f0f9196e434f224882dcdfe
prince301102 <79126372+prince301102@users.noreply.github.com>
2021-05-08 16:14:21 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -27,8 +27,12 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 **Hello {}, My Name is {}!** 
-I am an **SUPERB**  group management bot.
+
+I am Tera Baap Group management bot.
+
 You can find the list of available commands with /help.
+
+
 """
 
 HELP_STRINGS = """
