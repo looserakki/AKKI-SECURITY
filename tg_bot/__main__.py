051dc3d573abc8a92129d64f8f642017a20097f8
prince301102 <79126372+prince301102@users.noreply.github.com>
2021-05-08 15:54:48 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -138,16 +138,43 @@ def start(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_photo(
                 TECHNO_IMG,
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
-                                                                       callback_data="help_back".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/prince_3011")],
-                                                                                   [InlineKeyboardButton(text="⚜️Add In Group⚜️",
-                                                                       url="t.me/{}?startgroup=true".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="Source Code🗃️",
-                                                                       url="https://t.me/prince_music_chats")
-                                                                                 ]]))
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[
 
+                        InlineKeyboardButton(
+
+                            text="Tera Baap Add  to your group",
+
+                            url="t.me/{}?startgroup=true".format(
+
+                                context.bot.username))
+
+                    ],
+
+                     [
+
+                         InlineKeyboardButton(
+
+                             text="Support Group",
+
+                             url=f"https://t.me/prince_music_chats"),
+
+                         InlineKeyboardButton(
+
+                             text="📘 Logs",
+
+                             url="https://t.me/prince_music_chats")
+
+                     ],
+
+                     [
+
+                         InlineKeyboardButton(
+
+                             text="☑️ Source code",
+
+                             url="https://github.com/prince301102/TeraBaap")
+
+                     ]]))
     else:
         update.effective_message.reply_text("YES, I AM ONLINE.")
 
